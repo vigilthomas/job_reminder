e7391a191575489d407df543a9ddef245c546fc0
vigilthomas17 <vigilthomas17@gmail.com>
2023-12-01 13:27:16 +0530

    Models

--- joblisting/models.py
+++ joblisting/models.py
@@ -13,58 +13,55 @@ class Category(models.Model):
 
 class Jobs(models.Model):
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=200)
     salary = models.PositiveIntegerField()
     experience = models.PositiveIntegerField(default=0)
     last_date = models.DateField()
     vaccancies = models.PositiveIntegerField(default=1)
-    poster = models.ImageField(
-        upload_to="static/poster_img", null=True, blank=True)
-    contact = models.PositiveIntegerField()
-    qualifications = models.CharField(max_length=500)
+    poster = models.ImageField(upload_to="posterimages", null=True, blank=True)
+    contact = models.CharField(max_length=200)
+    qualification = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     status = models.BooleanField(default=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
 
 class StudentProfile(models.Model):
-    qualifications = models.CharField(max_length=500)
-    resume = models.FileField(
-        upload_to="static/resumes", null=True, blank=True)
+    qualification = models.CharField(max_length=200)
+    resume = models.FileField(upload_to="resumes", null=True, blank=True)
     skills = models.CharField(max_length=200)
     age = models.PositiveIntegerField()
     options = (
-        ("Male", "Male"),
-        ("Female", "Female"),
-        ("Others", "Others")
+        ("male", "male"), ("female", "female")
     )
-    gender = models.CharField(max_length=100, choices=options, default="Male")
+    gender = models.CharField(max_length=200, choices=options, default="male")
     experience = models.PositiveIntegerField(default=0)
-    address = models.PositiveIntegerField()
-    phone = models.PositiveIntegerField()
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
     profile_pic = models.ImageField(
-        upload_to="static/profile_pic", null=True, blank=True)
+        upload_to="profilepics", null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Applications(models.Model):
-    jobs = models.ForeignKey(Jobs, on_delete=models.DO_NOTHING)
+    job = models.ForeignKey(Jobs, on_delete=models.DO_NOTHING)
     student = models.ForeignKey(User, on_delete=models.CASCADE)
-    applied_date = models.DateTimeField(auto_now=True)
+    applied_date = models.DateTimeField(auto_now_add=True)
     options = (
-        ("Pending", "Pending"),
-        ("Rejected", "Rejected"),
+        ("pending", "pending"), ("rejected",
+                                 "rejected"), ("processing", "processing")
     )
-    status = models.CharField(max_length=100, choices=options, default="Pend")
+    status = models.CharField(
+        max_length=200, choices=options, default="pending")
 
 
 class Projects(models.Model):
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
-    git_link = models.CharField(max_length=1000)
-    user=models.ForeignKey(StudentProfile,on_delete=models.DO_NOTHING)
+    description = models.CharField(max_length=200)
+    git_link = models.CharField(max_length=200)
+    user = models.ForeignKey(StudentProfile, on_delete=models.DO_NOTHING)
 
-    def  __str__(self):
+    def __str__(self) -> str:
         return self.name
